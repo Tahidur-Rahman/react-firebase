196e18ea833d4dbd3eebb2785820286f54984051
Tahidur-rahman <mdtahidurrahman20@gmail.com>
2021-01-26 20:36:57 +0600

    route setup done using react-router-dom

--- src/Components/Navigation/index.js
+++ src/Components/Navigation/index.js
@@ -1,8 +1,26 @@
-import React from 'react';
-
+import React from "react";
+import {Link} from "react-router-dom";
+import * as ROUTES from "../../constants/routes";
+import './nav.css'
 const Navigation = () => (
   <div>
-    <h1>Navigation</h1>
+    <ul className="nav">
+      <li>
+        <Link to={ROUTES.SIGN_IN} className="link">Sign In</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.LANDING} className="link">Landing</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.HOME} className="link">Home</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.ACCOUNT} className="link">Account</Link>
+      </li>
+      <li>
+        <Link to={ROUTES.ADMIN} className="link">Admin</Link>
+      </li>
+    </ul>
   </div>
 );
 
