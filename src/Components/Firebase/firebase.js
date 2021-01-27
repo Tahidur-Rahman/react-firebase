fabb2a8532250045834eb5ff84f8782f034a32a6
Tahidur-rahman <mdtahidurrahman20@gmail.com>
2021-01-27 19:48:12 +0600

    react authentication setup

--- src/Components/Firebase/firebase.js
+++ src/Components/Firebase/firebase.js
@@ -1,20 +1,31 @@
-import app from 'firebase/app'
+import app from "firebase/app";
+import "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyApvOEYCSkDs5MNzkkwCl-qo1mmk9CaZKE",
-    authDomain: "react-firebase-tahid.firebaseapp.com",
-    projectId: "react-firebase-tahid",
-    storageBucket: "react-firebase-tahid.appspot.com",
-    messagingSenderId: "768068872246",
-    appId: "1:768068872246:web:0f74db32c23856d6471097",
-    measurementId: "G-S8HMC18B09"
-  };
-  
- 
-  class Firebase{
-      constructor(){
-          app.initializeApp(firebaseConfig)
-      }
-  }
+  apiKey: "AIzaSyApvOEYCSkDs5MNzkkwCl-qo1mmk9CaZKE",
+  authDomain: "react-firebase-tahid.firebaseapp.com",
+  projectId: "react-firebase-tahid",
+  storageBucket: "react-firebase-tahid.appspot.com",
+  messagingSenderId: "768068872246",
+  appId: "1:768068872246:web:0f74db32c23856d6471097",
+  measurementId: "G-S8HMC18B09",
+};
 
-  export default Firebase;
+class Firebase {
+  constructor() {
+    app.initializeApp(firebaseConfig);
+    this.auth = app.auth();
+  }
+  doCreateUserWithEmailAndPassword = (email, password) => {
+    this.auth.createUserWithEmailAndPassword(email, password);
+  };
+  doSignInWithEmailAndPassword = (email, password) => {
+    this.auth.signInWithEmailAndPassword(email, password);
+  };
+  doSignOut = () => this.auth.signOut();
+  doPasswordReset = (email) => this.auth.sendPasswordResetEmail(email);
+  doPasswordUpdate = (password) =>
+    this.auth.currentUser.updatePassword(password);
+}
+
+export default Firebase;
